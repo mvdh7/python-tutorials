7e13c04fc91007d43680897f0d67f24f1f1fa837
Matthew Humphreys <m.p.humphreys@icloud.com>
2021-01-29 11:41:13 +0100

    Create tutorial-0.py

---
+++ tutorial-0.py
@@ -0,0 +1,30 @@
+# Numbers
+x = 5.0
+print(x + 9)  # print puts the output in the console
+
+# Strings - pieces of text
+my_name = 'Matthew'
+best_animal = "penguin 'obviously'"
+number = "5"  # can't do maths with this!
+
+# Lists
+my_list = [1, 2, 3, 4, 5]
+list_two = ["Kristin", "Rob", "Maartje", 5]
+# Lists not good for maths
+times_two = my_list * 2
+both_lists = my_list + list_two
+
+# Access list items
+print(list_two[0])
+print(list_two[-1])  # final element
+
+# Dict(ionary)
+my_dict = {
+    "Netherlands": "Dutch",
+    "UK": "English",
+    "USA": "English",
+    "Germany": "German",
+    56: 123,
+}
+# Access dict items
+print(my_dict["UK"])
