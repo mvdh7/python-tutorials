09eb20425b62b025c72c9fc9ab7907b8e07dde90
Matthew Humphreys <m.p.humphreys@icloud.com>
2021-03-19 10:31:02 +0100

    Update t5-interpolating-profiles.py

--- t5-interpolating-profiles.py
+++ t5-interpolating-profiles.py
@@ -24,31 +24,31 @@ def cluster_profile(depth_values, x_values, bandwidth=10):
     depth_values_v = np.vstack(depth_values)
     clustering = MeanShift(bandwidth=bandwidth).fit(depth_values_v)
     cluster_labels = clustering.labels_
-    
+
     # Average by cluster
     depth_clusters = clustering.cluster_centers_.ravel()
     x_clusters = np.full_like(depth_clusters, np.nan)
     for i in range(len(depth_clusters)):
         x_clusters[i] = np.mean(x_values[cluster_labels == i])
-    
+
     # Sort arrays by depth
     depth_index = np.argsort(depth_clusters)
     x_clusters = x_clusters[depth_index]
     depth_clusters = depth_clusters[depth_index]
-    
+
     # # Average duplicate values
     # depth_unique = np.unique(depth_values)
     # x_unique = np.full_like(depth_unique, np.nan)
     # for i in range(len(depth_unique)):
     #     x_unique[i] = np.mean(x_values[depth_values == depth_unique[i]])
-    
+
     # Do a PCHIP interpolation
     interpolator = interpolate.pchip(depth_clusters, x_clusters)
-    depth_plotting = np.linspace(np.min(depth_values), np.max(depth_values),
-                                 num=1000)
+    depth_plotting = np.linspace(np.min(depth_values), np.max(depth_values), num=1000)
     x_plotting = interpolator(depth_plotting)
-    
+
     return depth_clusters, x_clusters, depth_plotting, x_plotting
+
 
 # Don't forget to run the function!
 depth_clusters, x_clusters, depth_plotting, x_plotting = cluster_profile(
